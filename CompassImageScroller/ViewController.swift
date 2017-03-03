022737be5bcf4579d8daeb7b5aa3cbcdf798788c
Liam Morley <lmorley@pivotal.io>
2017-03-02 21:09:12 -0500

    WIP - working on implementing the tutorial in a TDD manner as found here: http://www.codingexplorer.com/choosing-images-with-uiimagepickercontroller-in-swift/
    Signed-off-by: Mitchell Rosen <mrosen@pivotal.io>

--- CompassImageScroller/ViewController.swift
+++ CompassImageScroller/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func loadImageButtonTapped(_ sender: UIButton) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- CompassImageScrollerUITests/CompassImageScrollerUITests.swift
+++ CompassImageScrollerUITests/CompassImageScrollerUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class CompassImageScrollerUITests: XCTestCase {
+    var app: XCUIApplication!
         
     override func setUp() {
         super.setUp()
@@ -18,7 +19,8 @@ class CompassImageScrollerUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +30,18 @@ class CompassImageScrollerUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDisplaysImageSelectorDialogOnButtonPress() {
+        app.buttons["Load Image"].tap()
+        
+// tutorial: http://www.codingexplorer.com/choosing-images-with-uiimagepickercontroller-in-swift/
+// useful discussions:
+// http://stackoverflow.com/questions/25351394/update-position-of-uiimageview-swift
+// http://stackoverflow.com/questions/38591260/how-to-pick-an-image-at-some-index-from-uiimagepickercontroller-under-uitests-in
+// http://stackoverflow.com/questions/35294148/how-to-correctly-uitest-an-application-with-uiimagepickercontroller-or-any-othe
+        
+        
+//        app.buttons["Choose"].tap()
+        
     }
     
 }
